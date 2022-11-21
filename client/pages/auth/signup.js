cf1dadfad701c178617a98591d2d8cc47332eecc
Asmovic <Asogba007@gmail.com>
2022-11-21 15:20:53 +0100

    Add Signup feature

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default () => {
-    return <form className="container">
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const response = await axios.post("/api/users/signup", {
+            email, password
+        });
+
+        console.log(response.data);
+    }
+    return <form onSubmit={onSubmit} className="container">
         <h1>Sign up</h1>
         <div className="form-group">
             <label>Email Address</label>
-            <input className="form-control" />
+            <input value={email} onChange={(e)=> setEmail(e.target.value)} className="form-control" />
         </div>
-        <div className="form-group">
+        <div value={password} onChange={(e)=> setPassword(e.target.value)} className="form-group">
             <label>Password</label>
             <input type="password" className="form-control" />
         </div>
-        <button className="btn btn-primary">Sign Up</button>
+        <button type="submit" className="btn btn-primary mt-3">Sign Up</button>
     </form>
 }
