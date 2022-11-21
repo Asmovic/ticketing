de71923d867f7942d164321cda4e7aa88e5776be
Asmovic <Asogba007@gmail.com>
2022-11-21 22:15:23 +0100

    Add use-request hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,16 +1,16 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from "next/router";
+import useRequest from "../../hooks/use-request";
 
 export default () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const { doRequest, errors } = useRequest({ url: "/api/users/signup", method: "post", 
+    body: { email, password }, onSuccess: ()=> Router.push("/")})
+    
     const onSubmit = async (e) => {
         e.preventDefault();
-        const response = await axios.post("/api/users/signup", {
-            email, password
-        });
-
-        console.log(response.data);
+        doRequest();
     }
     return <form onSubmit={onSubmit} className="container">
         <h1>Sign up</h1>
@@ -18,10 +18,11 @@ export default () => {
             <label>Email Address</label>
             <input value={email} onChange={(e)=> setEmail(e.target.value)} className="form-control" />
         </div>
-        <div value={password} onChange={(e)=> setPassword(e.target.value)} className="form-group">
+        <div value={password} onChange={(e)=> setPassword(e.target.value)} className="form-group mb-3">
             <label>Password</label>
             <input type="password" className="form-control" />
         </div>
+        {errors}
         <button type="submit" className="btn btn-primary mt-3">Sign Up</button>
     </form>
 }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,22 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+    console.log("Currentuser...", currentUser)
     return <h1>Landing Page</h1>
 }
+
+LandingPage.getInitialProps = async ({ req }) => {
+    /* console.log(req.headers); */
+    if(typeof window === "undefined") {
+        const { data } = await axios.get(
+            "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser", {
+                headers: req.headers
+            });
+        return data;
+    } else {
+        const { data } = await axios.get("/api/users/currentuser");
+        return data;
+    }
+}
+
+export default LandingPage;
