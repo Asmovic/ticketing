3b2eab7cbab3fec72fa8978f9712869e4a8ccb11
Asmovic <Asogba007@gmail.com>
2022-11-24 15:29:31 +0100

    Finalize ticket create feature

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { requireAuth, validateRequest } from "@asmovictickets/common";
 import { body } from "express-validator";
+import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
@@ -12,8 +13,13 @@ router.post("/api/tickets", requireAuth, [
     .isFloat({ gt: 0 })
     .notEmpty()
     .withMessage("Price must be greater than zero")
-], validateRequest, (req:Request, res:Response)=>{
-    res.status(200).send({})
+], validateRequest, async (req:Request, res:Response)=>{
+    const { title, price } = req.body;
+    const ticket = Ticket.build({
+        title, price, userId: req.currentUser!.id
+    })
+    await ticket.save();
+    res.status(201).send(ticket)
 })
 
 export { router as createTicketRouter }
