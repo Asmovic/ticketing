6abbc6286504baa0fb61264836703c700e15f534
Asmovic <Asogba007@gmail.com>
2022-12-01 10:10:38 +0100

    Create order and ticket models

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,37 +1,19 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
 import { requireAuth, validateRequest } from "@asmovictickets/common";
 import { body } from "express-validator";
-import { Ticket } from "../models/ticket";
-import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
-import { natsWrapper } from "../nats-wrapper";
-import mongoose from "mongoose";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.post("/api/routes", requireAuth, [
+router.post("/api/orders", requireAuth, [
     body("tickedId")
     .notEmpty()
     .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
-    .withMessage("TicketId must be provided"),
-    body("price")
-    .isFloat({ gt: 0 })
-    .notEmpty()
-    .withMessage("Price must be greater than zero")
+    .withMessage("TicketId must be provided")
 ], validateRequest, async (req:Request, res:Response)=>{
-    const { title, price } = req.body;
-    const ticket = Ticket.build({
-        title, price, userId: req.currentUser!.id
-    })
-    await ticket.save();
-
-    await new TicketCreatedPublisher(natsWrapper.client).publish({
-        id: ticket.id,
-        title: ticket.title,
-        price: ticket.price,
-        userId: ticket.userId
-    });
-
-    res.status(201).send(ticket)
+    
+    res.status(201).send({})
 })
 
-export { router as createTicketRouter }
+export { router as createOrderRouter }
