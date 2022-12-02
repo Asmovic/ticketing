8da483045ae263ad3712fdcb451d310edd0c859b
Asmovic <Asogba007@gmail.com>
2022-12-02 08:54:51 +0100

    Add delete route

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -7,6 +7,7 @@ import { errorHander, NotFoundError, currentUser } from "@asmovictickets/common"
 import { indexOrderRouter } from "./routes/index";
 import { createOrderRouter } from "./routes/new";
 import { showOrderRouter } from "./routes/show";
+import { deleteOrderRouter } from "./routes/delete";
 
 const app = express();
 app.set("trust proxy", true);
@@ -22,6 +23,7 @@ app.use(currentUser);
 app.use(indexOrderRouter);
 app.use(createOrderRouter);
 app.use(showOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", async ()=>{
     throw new NotFoundError();
