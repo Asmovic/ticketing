163f2d2f8294502889a0f9661562ad7533b8ab96
Asmovic <Asogba007@gmail.com>
2022-12-06 11:17:24 +0100

    Add order created listener to payments service

--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -4,16 +4,16 @@ import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 
 interface OrderAttrs {
     id: string;
-    version: string;
+    version: number;
     userId: string;
-    price: string;
+    price: number;
     status: OrderStatus;
 }
 
 interface OrderDoc extends mongoose.Document {
-    version: string;
+    version: number;
     userId: string;
-    price: string;
+    price: number;
     status: OrderStatus;
 }
 
