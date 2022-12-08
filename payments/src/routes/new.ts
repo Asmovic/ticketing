bea265bd6f3faf88eb1db5a06de326f82fe900fc
Asmovic <Asogba007@gmail.com>
2022-12-08 08:00:49 +0100

    Add create payment route skaffold

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,6 +1,7 @@
-import { requireAuth, validateRequest } from "@asmovictickets/common";
+import { BadRequestError, currentUser, NotAuthorizedError, NotFoundError, OrderStatus, requireAuth, validateRequest } from "@asmovictickets/common";
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -8,10 +9,20 @@ router.post("/api/payments", requireAuth, [
     body("token").notEmpty(),
     body("orderId").notEmpty(),
 ], validateRequest, async (req: Request, res: Response) => {
-    
+    const { token, orderId } = req.body;
 
+    const order = await Order.findById(orderId);
 
-    res.send({ success: true })
+    if(!order) {
+        throw new NotFoundError();
+    }
+
+    if(req.currentUser!.id !== order.userId) {
+        return new NotAuthorizedError();
+    }
+    if(order.status === OrderStatus.Cancelled) {
+        throw new BadRequestError("Cannot pay for a cancelled order")
+    }
 });
 
 export { router as createChargeRouter }
